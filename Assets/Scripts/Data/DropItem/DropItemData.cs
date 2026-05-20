1f930c758ccae65c8ac81dad438e0478eb36df48
김유찬 <152022287+muchan918@users.noreply.github.com>
2026-05-20 16:45:20 +0900

    Delete DropItemData

--- Assets/Scripts/Data/DropItem/DropItemData.cs
+++
@@ -1,7 +0,0 @@
-public class DropItemData
-{
-    public string DropItemID { get; set; }
-    public string DisplayName { get; set; }
-    public string ItemType { get; set; }
-    public int StackMax { get; set; }
-}
