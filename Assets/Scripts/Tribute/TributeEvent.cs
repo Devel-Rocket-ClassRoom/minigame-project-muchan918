a171530bc60261c4ccd8d45d4b7af9a426528378
muchan <>
2026-06-04 01:59:37 +0900

    feat: Balance잡는중

--- Assets/Scripts/Tribute/TributeEvent.cs
+++ Assets/Scripts/Tribute/TributeEvent.cs
@@ -21,16 +21,6 @@ public class TributeEvent : MonoBehaviour
     public string CurrentRequirementID =>
         levelPool[CurrentEventLevel].requirements[CurrentRequirementIndex].requirementID;
 
-    private void Update()
-    {
-        // 임시 레벨업 테스트 코드
-        if (Keyboard.current.mKey.wasPressedThisFrame)
-        {
-            LevelUp();
-            Debug.Log($"[TributeEvent] 레벨업 → Level {TributeLevel}");
-        }
-    }
-
     public bool Evaluate()
     {
         if (tributeSlotList.IsAllComplete())
@@ -48,17 +38,29 @@ public class TributeEvent : MonoBehaviour
 
     public void AssignNewEvent()
     {
+        int sum = 0;
+        if (UpgradeManager.Instance != null)
+        {
+            sum =
+                UpgradeManager.Instance.GetLevel(UpgradeType.Workbench)
+                + UpgradeManager.Instance.GetLevel(UpgradeType.Animal)
+                + UpgradeManager.Instance.GetLevel(UpgradeType.Resource)
+                + UpgradeManager.Instance.GetLevel(UpgradeType.Cauldron);
+        }
+
+        TributeLevel = sum switch
+        {
+            <= 3 => 0,
+            <= 6 => 1,
+            <= 9 => 2,
+            _ => 3,
+        };
+
         IsTributeFulfilled = false;
         CurrentEventLevel = TributeLevel;
-        var pool = levelPool[CurrentEventLevel].requirements;
+        var pool = levelPool[TributeLevel].requirements;
         CurrentRequirementIndex = Random.Range(0, pool.Count);
         tributeSlotList.Setup(pool[CurrentRequirementIndex]);
-    }
-
-    public void LevelUp()
-    {
-        TributeLevel = Mathf.Min(TributeLevel + 1, levelPool.Count - 1);
-        Debug.Log($"[TributeEvent] 레벨업 → TributeLevel {TributeLevel}");
     }
 
     public void SetRequirement(int tributeLevel, int eventLevel, int index)
